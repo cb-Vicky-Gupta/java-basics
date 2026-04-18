e6f166bf05deb6ba9be43bfa02e261ae038e4d67
Vicky Gupta <vicky.k.g@thecodebucket.com>
2026-04-18 09:29:18 +0530

    optimize solution added

--- src/_01math/NumberOfDigits.java
+++ src/_01math/NumberOfDigits.java
@@ -2,8 +2,9 @@ package _01math;
 
 public class NumberOfDigits {
     static void main() {
-        System.out.println(withBaseN(10,2));
+        System.out.println(withBaseNOptimize(10,2));
     }
+    // brute force
     static public int withBaseN(int num, int base) {
         if (num == 0) return 1;
 
@@ -13,6 +14,11 @@ public class NumberOfDigits {
             count++;
         }
         return count;
+    }
+    static public int withBaseNOptimize(int num, int base) {
+       // formula int(log(base^num) +1 ===> int(log(num)/log(base)) + 1
+        int result = (int)(Math.log(num)/Math.log(base))+1;
+        return result;
     }
 }
 // 10 1010
