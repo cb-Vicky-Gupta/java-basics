a374d261f3701833fc2863f583791f6eb4706998
Vicky Gupta <vicky.k.g@thecodebucket.com>
2026-06-24 11:26:39 +0530

    added some questions

--- src/random/ArrayNew.java
+++ src/random/ArrayNew.java
@@ -7,63 +7,67 @@ import java.util.List;
 
 public class ArrayNew {
     static void main() {
-        int[] arr = {1,2,3,4};
-        int k =-2;
-        int [] arrN = decrypt(arr, k);
+        int[] arr = {1, 2, 3, 4};
+        int k = -2;
+        int[] arrN = decrypt(arr, k);
         for (int i = 0; i < arrN.length; i++) {
 //            System.out.print(arrN[i] + " ");
         }
-        System.out.println(maxProduct(arr));
+        System.out.println(reverseSubString("abcd", 'z'));
 
     }
+
     public static int maxVowels(String s, int k) {
-        List<Character> list = new ArrayList<>(Arrays.asList('a','e','i','o','u'));
+        List<Character> list = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         int count = 0;
-        for (int i = 0; i <k; i++) {
-            if(list.contains(s.charAt(i))){
+        for (int i = 0; i < k; i++) {
+            if (list.contains(s.charAt(i))) {
                 count++;
             }
         }
         int max = count;
         for (int i = k; i < s.length(); i++) {
 
-            if(list.contains(s.charAt(i))){
+            if (list.contains(s.charAt(i))) {
                 max++;
             }
-            if(list.contains(s.charAt(i-k))){
+            if (list.contains(s.charAt(i - k))) {
                 max--;
             }
             count = Math.max(max, count);
         }
         return count;
     }
+
     public static int findMaxConsecutiveOnes(int[] nums) {
         int count = 0;
         int reset = 0;
         for (int i = 0; i < nums.length; i++) {
 
-            if(nums[i] == 1){
+            if (nums[i] == 1) {
                 reset++;
-                if(count<reset){
+                if (count < reset) {
                     count++;
                 }
-            }else{
+            } else {
                 reset = 0;
             }
         }
         return count;
     }
+
     public static int[] decrypt(int[] code, int k) {
-        int [] arr = new int[code.length];
+        int[] arr = new int[code.length];
         for (int i = 0; i < code.length; i++) {
-            arr[i] = makeArraysum(code, k , i);
+            arr[i] = makeArraysum(code, k, i);
         }
         return arr;
     }
-    public static int makeArraysum(int [] arr, int k, int i){
+
+    public static int makeArraysum(int[] arr, int k, int i) {
         int sum = 0;
         int n = arr.length;
-        if(k == 0){
+        if (k == 0) {
             return sum;
         }
         if (k > 0) {
@@ -78,6 +82,7 @@ public class ArrayNew {
         }
         return sum;
     }
+
     public static int countGoodSubstrings(String s) {
         if (s.length() < 3) return 0;
 
@@ -103,49 +108,81 @@ public class ArrayNew {
 
         return count;
     }
-    public static int maxProduct(int [] nums){
-        if(nums.length <=2) return -1;
-        if(nums.length == 3) return nums[0]*nums[1]*nums[2];
-        int max1 = Integer.MIN_VALUE, max2 =Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+    public static int maxProduct(int[] nums) {
+        if (nums.length <= 2) return -1;
+        if (nums.length == 3) return nums[0] * nums[1] * nums[2];
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
         int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            if(max1 < nums[i]){
+            if (max1 < nums[i]) {
                 max3 = max2;
                 max2 = max1;
-                max1= nums[i];
-            } else if (max2<nums[i]) {
+                max1 = nums[i];
+            } else if (max2 < nums[i]) {
                 max3 = max2;
-                max2= nums[i];
-            }else if (max3 <nums[i]){
-                max3=nums[i];
+                max2 = nums[i];
+            } else if (max3 < nums[i]) {
+                max3 = nums[i];
             }
 
-            if(min1>nums[i]){
+            if (min1 > nums[i]) {
                 min2 = min1;
-                min1= nums[i];
-            }else if (min2>nums[i]){
-                min2= nums[i];
+                min1 = nums[i];
+            } else if (min2 > nums[i]) {
+                min2 = nums[i];
             }
         }
-        System.out.print(max1 + " " + max2 + " " + max3 + " " + min1 + " " + min2 + " " );
-        return Math.max(max1*max2*max3 , max1*min1*min2);
+        System.out.print(max1 + " " + max2 + " " + max3 + " " + min1 + " " + min2 + " ");
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
     }
 
-        public static int minSubArrayLen(int target, int[] nums) {
-            int left = 0;
-            int currentSum = 0;
-            int minLength = Integer.MAX_VALUE;
+    public static int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int currentSum = 0;
+        int minLength = Integer.MAX_VALUE;
 
-            for (int right = 0; right < nums.length; right++) {
-                currentSum += nums[right];
+        for (int right = 0; right < nums.length; right++) {
+            currentSum += nums[right];
 
-                while (currentSum >= target) {
-                    minLength = Math.min(minLength, right - left + 1);
-                    currentSum -= nums[left];
-                    left++;
-                }
+            while (currentSum >= target) {
+                minLength = Math.min(minLength, right - left + 1);
+                currentSum -= nums[left];
+                left++;
             }
-
-            return minLength == Integer.MAX_VALUE ? 0 : minLength;
         }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+
+    public static int[] seperateZero(int[] nums) {
+
+        int slow = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != 0){
+                nums[slow] = nums[i];
+                slow++;
+            }
+         }
+        for (int i = slow; i < nums.length; i++) {
+            nums[slow] = 0;
+        }
+        return nums;
+    }
+    public static String reverseSubString(String word, char ch){
+        String result = "";
+
+        int index = -1;
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == ch){
+                index = i;
+                break;
+            }
+        }
+        result = word.substring(0, index+1);
+        String reversedMiddle = String.valueOf(new StringBuilder(result).reverse());
+        result = reversedMiddle+ word.substring(index+1, word.length());
+        return index == -1 ? word : result;
+    }
+
 }
