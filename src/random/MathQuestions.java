ac121078f9d78ccf26a5f685f590d446cf567127
Vicky Gupta <vicky.k.g@thecodebucket.com>
2026-07-13 18:28:31 +0530

    one more question added

---
+++ src/random/MathQuestions.java
@@ -0,0 +1,56 @@
+package random;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MathQuestions {
+    static void main() {
+//        List<Integer> ans = primesUpTo(20);
+//        for (int i = 0; i <= ans.size(); i++) {
+//            System.out.print(ans.get(i)+ " ");
+//        }
+        int [][] mat = {{1,2,3},{5,17,7},{9,10,11}};
+        System.out.println(diagonalPrime(mat));
+    }
+    public static  List<Integer> primesUpTo(int n) {
+        boolean[] composite = new boolean[n + 1];
+        int count = 0;
+        List<Integer> primes = new ArrayList<>();
+
+        for (int p = 2; p <= n; p++) {
+            if (!composite[p]) {
+                primes.add(p);
+                count++;
+                for (long multiple = (long) p * p; multiple <= n; multiple += p) {
+                    composite[(int) multiple] = true;
+                }
+            }
+        }
+        System.out.println(count);
+        return primes;
+    }
+
+    static Boolean isPrime(int num){
+        if(num<2) return false;
+
+        for (int i = 2; i*i <= num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    public static int diagonalPrime(int[][] nums){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if((i==j && isPrime(nums[i][j]) )){
+                    max = Math.max(max,nums[i][j]);
+                } else if ((isPrime(nums[i][nums.length - i - 1]))) {
+                    max = Math.max(max,nums[i][nums.length - i - 1]);
+                }
+            }
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+
+}
+
